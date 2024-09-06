4f8b18fa4c5fe3c339b2995386bd5c85ce279498
Zahid <rkrazzkhan01731@gmail.com>
2024-09-06 21:32:23 +0800

    All Products created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Products from './components/Products'
+import { Outlet, Route, Router, Routes } from 'react-router-dom'
+import Cart from './components/Cart'
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <Products />
+      <Outlet />
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,43 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
 
+import App from "./App"
+import Products from './components/Products';
+import SingleProduct from './components/SingleProduct';
+import Cart from './components/Cart';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path:"/",
+        element: <Products />
+      },
+      {
+        path:"/product/:id",
+        element: <SingleProduct />
+      },
+      {
+        path:"/cart",
+        element: <Cart />
+      },
+      {
+        path:"/cart",
+        element: <Cart />
+      },
+    ]
+  },
+]);
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
